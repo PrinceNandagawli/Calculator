a170dfe6f89e20e631517770e73c244fddb91a78
Prince Nandagawli <princenandagawli100@gmail.com>
2024-01-25 23:47:21 +0530

    commit

---
+++ scipt.js
@@ -0,0 +1,16 @@
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+  }
+  
+  function calculate() {
+    var display = document.getElementById("display");
+    var result = eval(display.value);
+    display.value = result;
+  }
+  
+  function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+  
+  
---
+++ script1.js
@@ -0,0 +1,4 @@
+function openCalculator() {
+    document.getElementById("calculator").style.display = "block";
+    document.querySelector(".welcome-popup").style.display = "none";
+  }
